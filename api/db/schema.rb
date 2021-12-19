7b5f4429608e0445d060aab486ea109e3b616f53
horitomoyuki <eziltom0131@gmail.com>
2021-12-19 20:54:33 +0900

    Userモデルとテーブル作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_112851) do
+ActiveRecord::Schema.define(version: 2021_12_19_114727) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
